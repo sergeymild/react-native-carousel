4d6b5c8471e6787627c6de328a5193f20d41ca9e
SergeyMild <sergeymild@yandex.ru>
2021-11-24 09:50:10 +0200

    Update CarouselPackage.kt

--- android/src/main/java/com/reactnativecarousel/CarouselPackage.kt
+++ android/src/main/java/com/reactnativecarousel/CarouselPackage.kt
@@ -12,6 +12,6 @@ class CarouselPackage : ReactPackage {
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-        return listOf(CarouselViewManager())
+        return listOf(CarouselViewManager(), CarouselPageWrapperManager())
     }
 }
